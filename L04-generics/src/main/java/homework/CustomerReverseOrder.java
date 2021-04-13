669d38cbec2f07f881320eb26f1605c3fb0b4cc0
aa <johndoe@example.com>
2021-04-13 18:46:09 +0300

    Add HW 2: containers

---
+++ L04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -0,0 +1,17 @@
+package homework;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class CustomerReverseOrder {
+
+  private final Deque<Customer> customers = new ArrayDeque<>();
+
+  public void add(Customer customer) {
+    customers.push(customer);
+  }
+
+  public Customer take() {
+    return customers.pop();
+  }
+}
