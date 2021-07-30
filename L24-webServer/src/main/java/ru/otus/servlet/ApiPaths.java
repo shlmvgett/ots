eca9548010e678050725ae0825f081ec787b88cb
a_shalamov <a.shalamov@s7.ru>
2021-07-30 22:21:05 +0300

    Fix path consts

---
+++ L24-webServer/src/main/java/ru/otus/servlet/ApiPaths.java
@@ -0,0 +1,12 @@
+package ru.otus.servlet;
+
+public class ApiPaths {
+
+  private ApiPaths() {
+  }
+
+  public static String ROOT = "/";
+  public static String LOGIN = "/login";
+  public static String USERS = "/users";
+  public static String API_USERS = "/api/user/*";
+}
